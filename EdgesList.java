f0821cde9031c79af2f588586d24b330568796bd
CharlesRouillard <rouillard.charles@hotmail.com>
2017-09-25 15:55:50 +0200

    final updates with comments

--- EdgesList.java
+++ EdgesList.java
@@ -3,30 +3,50 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * 
+ * @authors ZEGHLACHE Adel & ROUILLARD Charles
+ *
+ */
 public class EdgesList {
+	
+	/**
+	 * 
+	 * @param args the file or pathname
+	 * @throws IOException if the file does not exist
+	 */
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("data/out.roadNet-CA"));
-		ArrayList<String> al = new ArrayList<String>();
-		try 
-		{
-			String line;
-			String[] tab;
-			while((line = br.readLine()) != null) {
-				if(!line.contains("%")) {
-					tab = line.split(" ");
-					al.add("(" + tab[0] + "," + tab[1] + ")");
+		if(args.length != 1) {
+			System.err.println("Need to pass a filename or a path in argument");
+		}
+		else {
+			String file = args[0];
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			ArrayList<String> al = new ArrayList<String>();
+			
+			/**
+			 * Store each node of each in line in an arrayList
+			 * if the first line is "x y" then it will store and prints '(x,y)'
+			 **/
+			try 
+			{
+				String line;
+				String[] tab;
+				while((line = br.readLine()) != null) {
+					if(!line.contains("%")) {
+						tab = line.split(" ");
+						al.add("(" + tab[0] + "," + tab[1] + ")");
+					}
 				}
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
 			}
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
+			br.close();
+			
+			for(int i = 0;i<al.size();i++) {
+				System.out.println(al.get(i));
+			}
 		}
-		br.close();
-		
-		for(int i = 0;i<al.size();i++) {
-			System.out.println(al.get(i));
-		}
-		
-		System.out.println("done");
 	}
 }
